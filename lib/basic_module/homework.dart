7ad35e71bc82dcf26ae189414719643f94cdb5c1
Som Sothea <somsothea@gmail.com>
2024-12-13 21:53:07 +0700

    Update homework

--- lib/basic_module/homework.dart
+++ lib/basic_module/homework.dart
@@ -36,63 +36,73 @@ class Post extends StatelessWidget {
     );
   }
 
- Widget _buildStack() {
-  return Stack(
-    children: [
-      Column(
-        mainAxisSize: MainAxisSize.min, // Adjust to fit the content height
-        children: [
-          Container(
-            width: 300,
-            height: 250,
-            decoration: BoxDecoration(
-              color: Colors.black,
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(20),
-                topRight: Radius.circular(20),
+  Widget _buildStack() {
+    return Stack(
+      children: [
+        Column(
+          mainAxisSize: MainAxisSize.min, // Adjust to fit the content height
+          children: [
+            Container(
+              width: 300,
+              height: 250,
+              decoration: BoxDecoration(
+                color: Colors.black,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(20),
+                  topRight: Radius.circular(20),
+                ),
               ),
+              alignment: Alignment.topCenter,
+              child: _buildImageRow(),
             ),
-            alignment: Alignment.topCenter,
-            child: _buildImageRow(),
-          ),
-          Container(
-            width: 300,
-            height: 200,
-            color: Colors.black,
-            alignment: Alignment.center,
-            child: _buildPara(),
-          ),
-          Container(
-            width: 300,
-            height: 50,
-            color: Colors.black,
-            alignment: Alignment.center,
-            child: _buildIconRow(),
-          ),
-        ],
-      ),
-      Positioned(
-        right: 20,
-        child: ElevatedButton(
-          onPressed: () {},
-          child: Text(
-            "Download",
-            style: TextStyle(color: Colors.white), // Ensure white text color
-          ),
-          style: ElevatedButton.styleFrom(
-            backgroundColor: Colors.red, // Background color
+            Container(
+              width: 300,
+              height: 200,
+              color: Colors.black,
+              alignment: Alignment.center,
+              child: _buildPara(),
+            ),
+            Container(
+              width: 300,
+              height: 50,
+              color: Colors.black,
+              alignment: Alignment.center,
+              child: _buildIconRow(),
+            ),
+          ],
+        ),
+        Positioned(
+          top: 230,
+          right: 20,
+          child: ElevatedButton(
+            onPressed: () {},
+            child: Text(
+              "Download",
+              style: TextStyle(color: Colors.white), // Ensure white text color
+            ),
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.red, // Background color
+            ),
           ),
         ),
-      ),
-    ],
-  );
-}
+      ],
+    );
+  }
 
   Widget _buildImageRow() {
     return Container(
       height: 300,
       width: 300,
-      child: Image.network(pic, fit: BoxFit.cover),
+      child: ClipRRect(
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(20),
+          topRight: Radius.circular(20),
+        ),
+        child: Image.network(
+          pic,
+          fit: BoxFit.cover,
+        ),
+      ),
     );
   }
 
@@ -104,11 +114,10 @@ class Post extends StatelessWidget {
           width: 300,
           height: 200,
           color: Colors.black,
-          padding: EdgeInsets.all(10), 
+          padding: EdgeInsets.all(10),
           child: Column(
-            crossAxisAlignment:
-                CrossAxisAlignment.start, 
-            mainAxisAlignment: MainAxisAlignment.center, 
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Text(
                 'Book Title',
@@ -118,12 +127,12 @@ class Post extends StatelessWidget {
                   fontWeight: FontWeight.bold,
                 ),
               ),
-              SizedBox(height: 10), 
+              SizedBox(height: 10),
               Text(
                 'ដំណើររយៈពេល ២០ឆ្នាំ របស់កម្ពុជា ក្នុងនាមជាសមាជិកអង្គការពាណិជ្ជកម្មពិភពលោក | Cambodia’s 20-Year Journey As A WTO Member ',
                 style: TextStyle(
                   color: Colors.white70,
-                  fontSize: 16, 
+                  fontSize: 16,
                 ),
               ),
             ],
@@ -133,27 +142,31 @@ class Post extends StatelessWidget {
     );
   }
 
-Widget _buildIconRow() {
-  return Row(
-    children: [
-      IconButton(
-        onPressed: () {},
-        icon: Icon(Icons.shopping_cart, color: Colors.white), // Set icon color to white
-      ),
-      IconButton(
-        onPressed: () {},
-        icon: Icon(Icons.favorite, color: Colors.white), // Set icon color to white
-      ),
-      IconButton(
-        onPressed: () {},
-        icon: Icon(Icons.bookmark, color: Colors.white), // Set icon color to white
-      ),
-      Spacer(),
-      IconButton(
-        onPressed: () {},
-        icon: Icon(Icons.info, color: Colors.white), // Set icon color to white
-      ),
-    ],
-  );
-}
+  Widget _buildIconRow() {
+    return Row(
+      children: [
+        IconButton(
+          onPressed: () {},
+          icon: Icon(Icons.shopping_cart,
+              color: Colors.white), // Set icon color to white
+        ),
+        IconButton(
+          onPressed: () {},
+          icon: Icon(Icons.favorite,
+              color: Colors.white), // Set icon color to white
+        ),
+        IconButton(
+          onPressed: () {},
+          icon: Icon(Icons.bookmark,
+              color: Colors.white), // Set icon color to white
+        ),
+        Spacer(),
+        IconButton(
+          onPressed: () {},
+          icon:
+              Icon(Icons.info, color: Colors.white), // Set icon color to white
+        ),
+      ],
+    );
+  }
 }
